d840851bec33ff95f6cac3d382c7b0abcc6243d3
Kevin Cuzner <kevin@kevincuzner.com>
2013-05-25 23:54:59 -0600

    Add initial function templates for linuxspi

--- avrdude/linuxspi.c
+++ avrdude/linuxspi.c
@@ -24,25 +24,183 @@
 #include "ac_cfg.h"
 
 #include "avrdude.h"
+#include "avr.h"
 
-#if HAVE_LINUXSPI
+#if HAVE_SPIDEV
+
+/**
+ * Linux Kernel SPI Drivers
+ * 
+ * Copyright (C) 2006 SWAPP
+ *      Andrea Paterniani <a.paterniani@swapp-eng.it>
+ * Copyright (C) 2007 David Brownell (simplification, cleanup)
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Data for the programmer
+ */
+
+
+
+/**
+ * Function Prototypes
+ */
+
+//interface - management
+static void linuxspi_setup(PROGRAMMER* pgm);
+static void linuxspi_teardown(PROGRAMMER* pgm);
+//interface - prog
+static int linuxspi_open(PROGRAMMER* pgm, char* port);
+static void linuxspi_close(PROGRAMMER* pgm);
+// dummy functions
+static void linuxspi_disable(PROGRAMMER * pgm);
+static void linuxspi_enable(PROGRAMMER * pgm);
+static void linuxspi_display(PROGRAMMER * pgm, const char * p);
+//universal
+static int linuxspi_initialize(PROGRAMMER* pgm, AVRPART* p);
+// SPI specific functions
+static int linuxspi_cmd(PROGRAMMER * pgm, unsigned char cmd[4], unsigned char res[4]);
+static int linuxspi_program_enable(PROGRAMMER * pgm, AVRPART * p);
+static int linuxspi_chip_erase(PROGRAMMER * pgm, AVRPART * p);
+static int linuxspi_paged_load(PROGRAMMER * pgm, AVRPART * p, AVRMEM * m,
+                                 unsigned int page_size,
+                                 unsigned int addr, unsigned int n_bytes);
+static int linuxspi_paged_write(PROGRAMMER * pgm, AVRPART * p, AVRMEM * m,
+                                  unsigned int page_size,
+                                  unsigned int addr, unsigned int n_bytes);
+static int linuxspi_set_sck_period(PROGRAMMER *pgm, double sckperiod);
+
+static void linuxspi_setup(PROGRAMMER* pgm)
+{
+}
+
+static void linuxspi_teardown(PROGRAMMER* pgm)
+{
+}
+
+static int linuxspi_open(PROGRAMMER* pgm, char* port)
+{
+    if (strcmp(port, "unknown") == 0) //unknown port
+    {
+        fprintf(stderr, "%s: error: No port specified. Port should point to an SPI interface.\n", progname);
+        exit(1);
+    }
+    
+    return -1;
+}
+
+static void linuxspi_close(PROGRAMMER* pgm)
+{
+}
+
+static void linuxspi_disable(PROGRAMMER* pgm)
+{
+    //do nothing
+}
+
+static void linuxspi_enable(PROGRAMMER* pgm)
+{
+    //do nothing
+}
+
+static void linuxspi_display(PROGRAMMER* pgm, const char* p)
+{
+    //do nothing
+}
+
+static int linuxspi_initialize(PROGRAMMER* pgm, AVRPART* p)
+{
+    return -1;
+}
+
+static int linuxspi_cmd(PROGRAMMER* pgm, unsigned char cmd[4], unsigned char res[4])
+{
+    return -1;
+}
+
+static int linuxspi_program_enable(PROGRAMMER* pgm, AVRPART* p)
+{
+    return -1;
+}
+
+static int linuxspi_chip_erase(PROGRAMMER* pgm, AVRPART* p)
+{
+    return -1;
+}
+
+static int linuxspi_paged_load(PROGRAMMER* pgm, AVRPART* p, AVRMEM* m, 
+                               unsigned int page_size, unsigned int addr, unsigned int n_bytes)
+{
+    return -1;
+}
+
+static int linuxspi_paged_write(PROGRAMMER* pgm, AVRPART* p, AVRMEM* m,
+                                unsigned int page_size, unsigned int addr, unsigned int n_bytes)
+{
+    return -1;
+}
+
+static int linuxspi_set_sck_period(PROGRAMMER* pgm, double sckperiod)
+{
+    return -1;
+}
 
 void linuxspi_initpgm(PROGRAMMER * pgm)
 {
-  fprintf(stderr,
-	  "%s: Linux SPI driver not available in this configuration\n",
-	  progname);
+    strcpy(pgm->type, "linuxspi");
+    
+    /*
+     * mandatory functions
+     */
+
+    pgm->initialize     = linuxspi_initialize;
+    pgm->display        = linuxspi_display;
+    pgm->enable         = linuxspi_enable;
+    pgm->disable        = linuxspi_disable;
+    pgm->program_enable = linuxspi_program_enable;
+    pgm->chip_erase     = linuxspi_chip_erase;
+    pgm->cmd            = linuxspi_cmd;
+    pgm->open           = linuxspi_open;
+    pgm->close          = linuxspi_close;
+    pgm->read_byte      = avr_read_byte_default;
+    pgm->write_byte     = avr_write_byte_default;
+
+    /*
+     * optional functions
+     */
+
+    pgm->paged_write    = linuxspi_paged_write;
+    pgm->paged_load     = linuxspi_paged_load;
+    pgm->setup          = linuxspi_setup;
+    pgm->teardown       = linuxspi_teardown;
+    pgm->set_sck_period = linuxspi_set_sck_period;
 }
 
-const char linuxspi_desc[] = "SPI using Linux spidev driver (not available)";
+const char linuxspi_desc[] = "SPI using Linux spidev driver";
 
 #else
 
 void linuxspi_initpgm(PROGRAMMER * pgm)
 {
-  fprintf(stderr,
-	  "%s: Linux SPI driver not available in this configuration\n",
-	  progname);
+    fprintf(stderr,
+      "%s: Linux SPI driver not available in this configuration\n",
+      progname);
 }
 
 const char linuxspi_desc[] = "SPI using Linux spidev driver (not available)";
